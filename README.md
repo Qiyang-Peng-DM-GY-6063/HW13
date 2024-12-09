15feba8cd67c6f92420bce377e6df1ef1e840e3d
potuitou <qpeng@risd.edu>
2024-12-09 15:47:38 -0500

    final

--- README.md
+++ README.md
@@ -1,17 +1,8 @@
-# HW13 Template
+# HW13
 
-Arduino reads a button on pin 2 and a potentiometer on pin A0 and passes those values on to the p5js sketch via a Serial connection.
+Arduino reads potentiometer which act as the slider to change the threshold for color-changing of the painting.
 
-The object that is created and sent to p5js looks like this:
+Sketch
+ ![Sketch](./sketch.jpg)
 
-```
-data = {
-  A0: { value: integer },
-  D2: {
-    isPressed: boolean,
-    count: integer
-  }
-}
-```
-
-In p5js `A0.value` is used to change ellipse size, `D2.isPressed` is used to add new ellipses to an array and `D2.count` is used to change the color of new ellipses.
+https://drive.google.com/file/d/1g4u6Kl85LFRi0BNeSH3DF5jfPJNmlN4j/view?usp=sharing
--- sketch.js
+++ sketch.js
@@ -1,85 +1,123 @@
 // serial variables
 let mSerial;
-
 let connectButton;
 
-let readyToReceive;
+let potVal = 0; 
 
-// project variables
-let mElls = [];
+// image variables
+let oImg;
+let mImg;
+let x;
+let y;
 
-function receiveSerial() {
-  let line = mSerial.readUntil("\n");
-  trim(line);
-  if (!line) return;
+let RED = { r: 255, g: 0, b: 0 };
+let YELLOW = { r: 255, g: 255, b: 0 };
+let BLUE = { r: 0, g: 0, b: 255 };
 
-  if (line.charAt(0) != "{") {
-    print("error: ", line);
-    readyToReceive = true;
-    return;
+function preload() {
+  oImg = loadImage("mondriaan.jpg");
+  mImg = loadImage("mondriaan.jpg");
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+  oImg.resize(0, height);
+  mImg.resize(0, height);
+
+  x = (width - oImg.width) / 2;
+  y = (height - oImg.height) / 2;
+
+  oImg.loadPixels();
+
+  // setup serial
+  mSerial = createSerial(); 
+  connectButton = createButton("Connect To Serial");
+  connectButton.position(width / 2, height / 2);
+  connectButton.mousePressed(connectToSerial);
+
+  print("Setup complete");
+}
+
+function draw() {
+  background(0);
+
+  if (mSerial.opened()) {
+    mSerial.write(0xAB);
   }
 
-  // get data from Serial string
-  let data = JSON.parse(line).data;
-  let a0 = data.A0;
-  let d2 = data.D2;
-
-  // use data to update project variables
-  if (d2.isPressed) {
-    mElls.push({
-      x: random(width),
-      y: random(height),
-      c: map(d2.count % 20, 0, 20, 155, 255),
-      d: map(a0.value, 0, 4095, 20, 200),
-    });
+  if (mSerial.opened() && mSerial.availableBytes() > 0) {
+    receiveSerial();
   }
 
-  // serial update
-  readyToReceive = true;
+  let SIMILARITY_VALUE = map(potVal, 0, 4095, 0, 255);
+
+  mImg.loadPixels();
+  for (let idx = 0; idx < oImg.pixels.length; idx += 4) {
+    let redVal = oImg.pixels[idx];
+    let greenVal = oImg.pixels[idx + 1];
+    let blueVal = oImg.pixels[idx + 2];
+    let alphaVal = oImg.pixels[idx + 3];
+
+    if (similar(redVal, greenVal, blueVal, RED, SIMILARITY_VALUE)) {
+      mImg.pixels[idx] = 100;
+      mImg.pixels[idx + 1] = 100;
+      mImg.pixels[idx + 2] = 0;
+      mImg.pixels[idx + 3] = 15;
+    } else if (similar(redVal, greenVal, blueVal, YELLOW, SIMILARITY_VALUE)) {
+      mImg.pixels[idx] = 0;
+      mImg.pixels[idx + 1] = 100;
+      mImg.pixels[idx + 2] = 100;
+      mImg.pixels[idx + 3] = 15;
+    } else if (similar(redVal, greenVal, blueVal, BLUE, SIMILARITY_VALUE)) {
+      mImg.pixels[idx] = 100;
+      mImg.pixels[idx + 1] = 0;
+      mImg.pixels[idx + 2] = 100;
+      mImg.pixels[idx + 3] = 15;
+    } else {
+      mImg.pixels[idx] = redVal;
+      mImg.pixels[idx + 1] = greenVal;
+      mImg.pixels[idx + 2] = blueVal;
+      mImg.pixels[idx + 3] = alphaVal;
+    }
+  }
+  mImg.updatePixels();
+
+  image(mImg, x, y);
 }
 
 function connectToSerial() {
   if (!mSerial.opened()) {
     mSerial.open(9600);
-
-    readyToReceive = true;
     connectButton.hide();
+    print("Connected to serial port");
   }
 }
 
-function setup() {
-  // setup project
-  createCanvas(windowWidth, windowHeight);
+function receiveSerial() {
+  let line = mSerial.readUntil("\n");
+  if (!line) return;
 
-  // setup serial
-  readyToReceive = false;
+  line = trim(line);
 
-  mSerial = createSerial();
+  // Print raw
+  print("Received line:", line);
 
-  connectButton = createButton("Connect To Serial");
-  connectButton.position(width / 2, height / 2);
-  connectButton.mousePressed(connectToSerial);
+  if (line.length === 0) return;
+
+  let val = parseInt(line, 10);
+
+  if (!isNaN(val)) {
+    //assign portVal
+    potVal = constrain(val, 0,4095);
+  } else {
+    print("Received non-numeric data:", line);
+  }
+  
 }
 
-function draw() {
-  // project logic
-  background(0);
-
-  for (let i = 0; i < mElls.length; i++) {
-    let me = mElls[i];
-    fill(me.c, 0, 0);
-    ellipse(me.x, me.y, me.d, me.d);
-  }
-
-  // update serial: request new data
-  if (mSerial.opened() && readyToReceive) {
-    readyToReceive = false;
-    mSerial.clear();
-    mSerial.write(0xab);
-  }
-
-  // update serial: read new data
-  if (mSerial.availableBytes() > 8) {
-    receiveSerial();
-  }
+function similar(r, g, b, color, gap) {
+  return (
+    abs(r - color.r) < gap && abs(g - color.g) < gap && abs(b - color.b) < gap
+  );
 }
